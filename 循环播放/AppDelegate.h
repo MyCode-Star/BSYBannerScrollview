2929630ba4572366b01fb0362c693e1839fdd7a4
mac <mac@mac.local>
2015-04-17 16:42:24 +0800

    Initial Commit

---
+++ 循环播放/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  循环播放
+//
+//  Created by mac on 15/4/17.
+//  Copyright (c) 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 循环播放/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  循环播放
+//
+//  Created by mac on 15/4/17.
+//  Copyright (c) 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
